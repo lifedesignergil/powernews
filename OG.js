19ef9a265e8fe1b8582d7a6f715ac587742d588e
lifedesignergil <kbd8611@gmail.com>
2023-09-01 19:04:55 +0900

    OG test

--- OG.js
+++ OG.js
@@ -1,14 +1,29 @@
-return res.json({});
-ogs({ url: url }, function(err, og_result) { // eslint-disable-line
- summary.summarize(url, function(tldr_result) {
-  var response = {
-  };
-  if (og_result && og_result.data) {
-   if (og_result.data.ogTitle) {
-    response.title = og_result.data.ogTitle;
-   if (og_result.data.ogDescription) {
-    response.description = og_result.data.ogDescription;
-   if (og_result.data.ogImage && og_result.data.ogImage.url) {
-    response.image = og_result.data.ogImage.url;
-   if (og_result.data.twitterImage && og_result.data.twitterImage.url) {
-    response.thumbnail = og_result.data.twitterImage.url;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class OpenGraphPreview {
+
+    public static void main(String[] args) {
+        String url = "https://wannafelx.com/176";
+
+        try {
+            // 웹 페이지의 HTML 가져오기
+            Document doc = Jsoup.connect(url).get();
+
+            // Open Graph 메타 데이터 추출
+            Elements metaTags = doc.select("meta[property^=og:]");
+            for (Element metaTag : metaTags) {
+                String property = metaTag.attr("property");
+                String content = metaTag.attr("content");
+                System.out.println(property + ": " + content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
